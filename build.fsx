e80e5b0c0329d2a659a6115bcbddf8af8ab4a05a
Erik Novales <enovales@codehitmen.com>
2017-05-15 20:31:02 -0700

    can build now

--- build.fsx
+++ build.fsx
@@ -4,38 +4,23 @@
 
 #r @"packages/build/FAKE/tools/FakeLib.dll"
 open Fake
-open Fake.Git
-open Fake.AssemblyInfoFile
-open Fake.ReleaseNotesHelper
 open Fake.UserInputHelper
 open System
 open System.IO
-#if MONO
-#else
-#load "packages/build/SourceLink.Fake/tools/Fake.fsx"
-open SourceLink
-#endif
 
 // The name of the project
-// (used by attributes in AssemblyInfo, name of a NuGet package and directory in 'src')
+// (used by directory in 'src')
 let project = "fsharp-slackbot-dojo"
 
 // Short summary of the project
-// (used as description in AssemblyInfo and as a short summary for NuGet package)
+// (used as description in AssemblyInfo)
 let summary = "Slack integration written with F#"
 
 // File system information
-let solutionFile  = "feltlion.sln"
+let solutionFile  = "fsharp-slackbot-dojo.sln"
 
 // Default target configuration
 let configuration = "Debug"
-
-// --------------------------------------------------------------------------------------
-// END TODO: The rest of the file includes standard build steps
-// --------------------------------------------------------------------------------------
-
-// Read additional information from the release notes document
-let release = LoadReleaseNotes "RELEASE_NOTES.md"
 
 // Helper active pattern for project types
 let (|Fsproj|Csproj|Vbproj|Shproj|) (projFileName:string) =
@@ -49,7 +34,9 @@ let (|Fsproj|Csproj|Vbproj|Shproj|) (projFileName:string) =
 // Copies binaries from default VS location to expected bin folder
 // But keeps a subdirectory structure for each project in the
 // src folder to support multiple project outputs
-Target "CopyBinaries" (fun _ ->
+Target "CopyBinaries" (fun () ->
+    ensureDirectory "bin"
+
     !! "src/**/*.??proj"
     -- "src/**/*.shproj"
     |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) </> "bin" </> configuration, "bin" </> (System.IO.Path.GetFileNameWithoutExtension f)))
@@ -66,7 +53,7 @@ let vsProjProps =
     [ ("Configuration", configuration); ("Platform", "Any CPU") ]
 #endif
 
-Target "Clean" (fun _ ->
+Target "Clean" (fun () ->
     !! solutionFile |> MSBuildReleaseExt "" vsProjProps "Clean" |> ignore
     CleanDirs ["bin"; "temp"; "docs/output"]
 )
@@ -74,18 +61,22 @@ Target "Clean" (fun _ ->
 // --------------------------------------------------------------------------------------
 // Build library & test project
 
-Target "Build" (fun _ ->
-    !! solutionFile
-    |> MSBuildReleaseExt "" vsProjProps "Rebuild"
-    |> ignore
-)
-
-// --------------------------------------------------------------------------------------
-// Run all targets by default. Invoke 'build <Target>' to override
-
 Target "All" DoNothing
 
-"Clean"
-  ==> "Release"
+Target "BuildBinaries" (fun () ->
+    let res = 
+      !! solutionFile
+      |> MSBuildReleaseExt "" vsProjProps "Rebuild"
+      
+    trace(String.Join("\n", res))
+    ()
+)
+
+Target "Build" (fun () -> ())
+
+"BuildBinaries"
+  ==> "Build"
+"CopyBinaries"
+  ==> "Build"  
 
 RunTargetOrDefault "All"
